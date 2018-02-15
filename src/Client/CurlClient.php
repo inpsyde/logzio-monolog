cbd760a63eedbb5941f8e0afd1102a7090b447a2
ChriCo <c.brueckner@inpsyde.com>
2018-02-15 13:59:59 +0100

    splitted transport and client into own classes to implement lateron Udp, TCP, ...

---
+++ src/Client/CurlClient.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+namespace Inpsyde\LogzIoMonolog\Client;
+
+use Monolog\Handler\Curl\Util;
+
+class CurlClient implements ClientInterface
+{
+
+    /**
+     * @var string
+     */
+    protected $endpoint;
+
+    /**
+     * @var array
+     */
+    protected $context;
+
+    /**
+     * @var int
+     */
+    protected $connectTimeout = 30;
+
+    public function __construct(string $endpoint)
+    {
+        $this->endpoint = $endpoint;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function write(string $data): bool
+    {
+        $resource = curl_init();
+        curl_setopt($resource, CURLOPT_URL, $this->endpoint);
+        curl_setopt($resource, CURLOPT_POST, true);
+        curl_setopt($resource, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($resource, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        curl_setopt($resource, CURLOPT_RETURNTRANSFER, true);
+        Util::execute($resource);
+
+        return true;
+    }
+}
